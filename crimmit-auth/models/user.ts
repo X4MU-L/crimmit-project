2c67ba24a5bb63d4b1fbc8acd9ab17eb73a9a8ac
chukwuebuka <okolisamuel21@gmail.com>
2024-08-28 23:58:07 +0100

    (feat) -> create databse model for user

---
+++ crimmit-auth/models/user.ts
@@ -0,0 +1,23 @@
+import { Document, Model, model, Schema } from "mongoose";
+
+export interface UserType extends Document {
+  username: string;
+  email: string;
+  password: string;
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+export type UserTypeWithoutPassword = Omit<UserType, "password">;
+
+export const userSchema = new Schema<UserType>({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User: Model<UserType> = model<UserType>("User", userSchema);
+
+export default User;
