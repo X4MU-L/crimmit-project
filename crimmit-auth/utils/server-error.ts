63706486bcadf424f8074d46faf19103a275701e
chukwuebuka <okolisamuel21@gmail.com>
2024-08-29 00:28:35 +0100

    (chore) -> create server error class

---
+++ crimmit-auth/utils/server-error.ts
@@ -0,0 +1,17 @@
+class ServerError extends Error {
+  public statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+
+    if (typeof Error.captureStackTrace === "function") {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new Error(message).stack;
+    }
+  }
+}
+
+export default ServerError;
