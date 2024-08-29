e594f9ad3828048305b92a1725fd13115f988cc6
chukwuebuka <okolisamuel21@gmail.com>
2024-08-29 20:36:52 +0100

    (chore) -> include types and public folder

--- crimmit-auth/routes/auth.ts
+++ crimmit-auth/routes/auth.ts
@@ -1,9 +1,51 @@
 import express from "express";
+import { asyncHandler, customAsyncHandler } from "../utils";
+import { authHandler } from "../middleware";
 
 const router = express.Router();
 
-import { handleSignUpUserRoute } from "../controllers";
+export interface resetPasswordSchema {
+  email?: string;
+  username?: string;
+}
+import {
+  handleSignUpUserRoute,
+  handleSignInRoute,
+  resetPasswordRoute,
+  resetPasswordFormRoute,
+  handlePasswordRoute,
+  handleRefreshTokenRoute,
+  handleSignOut,
+} from "../controllers";
 
-router.post("/signup", handleSignUpUserRoute);
+router.post("/signup", asyncHandler(handleSignUpUserRoute));
+router.post("/signin", asyncHandler(handleSignInRoute));
+router.post("/signout", asyncHandler(handleSignOut));
+router.post(
+  "/reset-password",
+  customAsyncHandler(authHandler),
+  customAsyncHandler(resetPasswordRoute)
+);
 
+router
+  .route("/reset-password/:token")
+  .get(
+    customAsyncHandler(authHandler),
+    customAsyncHandler(resetPasswordFormRoute)
+  )
+  .post(
+    customAsyncHandler(authHandler),
+    customAsyncHandler(handlePasswordRoute)
+  );
+
+router
+  .route("/refresh-token")
+  .get(
+    customAsyncHandler(authHandler),
+    customAsyncHandler(handleRefreshTokenRoute)
+  )
+  .post(
+    customAsyncHandler(authHandler),
+    customAsyncHandler(handleRefreshTokenRoute)
+  );
 export default router;
